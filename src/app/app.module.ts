bb7f3fe778617f4c516a2cdc84a4975f2c18496b
mrittik9320 <mrittik9320@gmail.com>
2024-11-26 13:26:42 +0530

    pkg chage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './test/test.component';
+import { MultiFileComponent } from './multi-file/multi-file.component';
+import { SingFileComponent } from './sing-file/sing-file.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 import { OneFromComponent } from './one-from/one-from.component';
+
+
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule} from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 //Materials Imports----------------------------------------------------------------
@@ -14,14 +21,13 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MultiFileComponent } from './multi-file/multi-file.component';
-import { SingFileComponent } from './sing-file/sing-file.component';
 import {MatInputModule} from '@angular/material/input';
-import { HomeComponent } from './home/home.component';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatCheckboxModule} from '@angular/material/checkbox';
-import { TestComponent } from './test/test.component';
+
+
+
+
 
 @NgModule({
   declarations: [
